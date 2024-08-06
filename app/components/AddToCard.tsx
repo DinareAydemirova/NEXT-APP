5e5c4e790ca8fdc5e4953770e89095df6b2b046c
DinareAydemirova <dinaraaydamirova@gmail.com>
2024-08-06 16:45:26 +0400

    next

---
+++ app/components/AddToCard.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const AddToCard = () => {
+  return (
+    <button onClick={()=>console.log('click')}>
+        Add to cart
+        </button>
+  )
+}
+
+export default AddToCard
